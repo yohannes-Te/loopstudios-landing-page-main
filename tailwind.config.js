69e7a1ca6fb18c9fd1838e8786b52dced50ad2b5
yohannes <yohannes22t@gmail.com>
2024-03-22 15:31:24 +0300

    initial loopstudios landing page commits

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+//     colors:{
+//       White: hsl(0 0% 100%),
+// Black: hsl(0, 0%, 0%)
+// DarkGray: hsl(0, 0%, 55%)
+// VeryDarkGray: hsl(0, 0%, 41%)
+//     },
+    extend: {
+
+     letterSpacing:{
+      widest:'.3em'
+     },
+    },
+  },
+  plugins: [],
+}
+
